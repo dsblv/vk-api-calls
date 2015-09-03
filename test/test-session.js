597f662755c7ed4e39af5d28c15bb16b67c43734
Dmitriy Sobolev <disobolev@icloud.com>
2015-09-03 12:43:07 +0300

    tests for eternal token

--- test/test-session.js
+++ test/test-session.js
@@ -24,21 +24,30 @@ test('token validation', function (t) {
 		'expires_in': 3600
 	};
 
-	var badData = {
+	var eternalData = {
 		'access_token': 'ACCESS_TOKEN',
 		'user_id': 'USER_ID',
 		'expires_in': 0
 	};
 
+	var badData = {
+		'access_token': 'ACCESS_TOKEN',
+		'user_id': 'USER_ID',
+		'expires_in': -1
+	};
+
 	var vk = new VK();
 
-	t.plan(2);
+	t.plan(3);
 
 	vk.setSession(goodData);
-	t.ok(vk.hasValidToken(), '#hasValidToken() retutns true when token isn\'t expired');
+	t.ok(vk.hasValidToken(), '#hasValidToken() retutns TRUE when token isn\'t expired');
+
+	vk.setSession(eternalData);
+	t.ok(vk.hasValidToken(), '#hasValidToken() retutns TRUE when token is eternal');
 
 	vk.setSession(badData);
-	t.ok(!vk.hasValidToken(), '#hasValidToken() retutns false when token is expired');
+	t.notOk(vk.hasValidToken(), '#hasValidToken() retutns FALSE when token is expired');
 
 	t.end();
 });
